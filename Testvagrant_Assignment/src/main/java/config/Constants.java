48727e12214e18503e56e3e0da20ac982de3a2eb
pravinkumbhare <pravin.kumbhare1@gmail.com>
2021-11-23 23:12:11 +0530

    Updating Constants.java file

---
+++ Testvagrant_Assignment/src/main/java/config/Constants.java
@@ -0,0 +1,16 @@
+package config;
+
+/**
+ * Created by pravin.kumbhare on 30-10-2021.
+ */
+public interface Constants {
+
+    public static final int TIME_IN_SECONDS = 30;
+    public static final String CITY_NAME = "Nagpur";
+    public static final String STATE = "Maharashtra";
+    public static String TEMPERATURE_TYPE = "FahrenheitToCelsius";
+    public static int STATUS_CODE_SUCCESS = 200;
+
+    public static final String WEB_PLATFORM = "WEB_PLATFORM";
+    public static final String API_PLATFORM = "API_PLATFORM";
+}
